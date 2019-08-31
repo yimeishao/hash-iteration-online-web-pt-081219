4cbc3d9ddf6238162d9fa27074a7adc981fe13ea
Maria Yimei Shao <maria.yimei.shao@gmail.com>
2019-08-31 00:22:12 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -15,5 +15,8 @@ def age_appropriate_birthday(birthday_kids)
     if age < 12 
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   else 
-    puts "You are too old for this"
+    puts "You are too old for this."
+  end 
+end
+end
     
